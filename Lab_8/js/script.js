9091076e1b681d1004d73888f12e384bda23bebb
1palya0 <85746832+1palya0@users.noreply.github.com>
2021-08-11 22:48:20 +0300

    Add files via upload

---
+++ Lab_8/js/script.js
@@ -0,0 +1,13 @@
+function task1(e) {
+    document.getElementById('p1').innerText = "X:" + e.pageX + " Y:" + e.pageY;
+}
+function task2() {
+    document.getElementById('body1').style.backgroundColor = "yellow";
+    document.getElementById('body1').style.backgroundImage = "url('https://www.meme-arsenal.com/memes/f38be88b6986181c801390813e707f4d.jpg')";
+    document.getElementById('body1').style.backgroundRepeat = "no-repeat";
+    document.getElementById('body1').style.backgroundPosition = "center";
+}
+function task3() {
+    document.getElementById('body1').style.backgroundColor = "white";
+    document.getElementById('body1').style.backgroundImage = "url('https://i.pinimg.com/originals/5e/ba/4e/5eba4e6675cdf9a54ae806e57e5a2784.jpg')";
+}
